8c159e4660260cec8f1db5f57f128ccb2536ee0d
Paulo Gois <pauloemanuel127@gmail.com>
2026-04-20 22:29:37 -0300

    Now the calculator receives the input on a single line

--- Calculadora.py
+++ Calculadora.py
@@ -7,35 +7,37 @@ while True:
     #O comando try é um comando feito para verificar o funcionamento do codigo ao longo dele
     #Nesse caso aqui ele serve para verificar se o usuario está realmente digitando valores nas entradas das variaveis X e Y
     try:
-        X = float(input('Digite o primeiro valor \n'))
-        Y = float(input('Digite o segundo valor \n'))
-        #Se o codigo chegou nessa parte é porque o usuario digitou valores numericos para as duas variaveis
-        operador = input('Escolha a operação a ser realizada, Digite:\n+ para soma\n- para subtração\n* para multiplicação\n/ para divisão\nexit para sair\n')
+        entrada = input("Digite a expressão a ser calculada no seguinte formato X + Y, tendo como opção as seguintes operações:\n+ para soma\n- para subtração\n* para multiplicação\n/ para divisão\nDigite apenas:\nexit para sair\n")
         #Após a escolha do operador entramos numa cadeia de ifs, onde será realizada a operação deseja ou o encerramento do funcionamento do codigo
-        if operador == '+':
-            valor = X+Y
-            print(f'Esse é o resultado da soma: {valor:.2f}')
-        elif operador == '-':
-            valor = X-Y
-            print(f'Esse é o resultado da subtração: {valor:.2f}')
-        elif operador == '*':
-            valor = X*Y
-            print(f'Esse é o resultado da multiplicação: {valor:.2f}')
-        elif operador == '/':
-            #Essa cadeia de condicional alinhada serve para caso onde o usuario tente fazer divisão por 0 e o codigo não falhe
-            if X == 0  and Y == 0:
-                print('Indeterminação')
-            elif Y == 0:
-                print('Indefinido')
-            else:
-                valor = X/Y
-                print(f'Esse é o resultado da divisão {valor:.2f}')
-        elif operador == 'exit':
+        if entrada == 'exit':
             print('Obrigado por usar nossa calculadora')
             break
         else:
-            #O else está aqui para caso o usuario use um operador que não seja alguma das opções oferecidas
-            print('Esse é um operador invalido, por favor tente novamente!')
+            entrada =  entrada.split()
+            X = float(entrada[0])
+            operador = str(entrada[1])
+            Y = float(entrada[2])
+            if operador == '+':
+                valor = X+Y
+                print(f'Esse é o resultado da soma: {valor:.2f} \n')
+            elif operador == '-':
+                valor = X-Y
+                print(f'Esse é o resultado da subtração: {valor:.2f} \n')
+            elif operador == '*':
+                valor = X*Y
+                print(f'Esse é o resultado da multiplicação: {valor:.2f} \n')
+            elif operador == '/':
+                #Essa cadeia de condicional alinhada serve para caso onde o usuario tente fazer divisão por 0 e o codigo não falhe
+                if X == 0  and Y == 0:
+                    print('Indeterminação \n')
+                elif Y == 0:
+                    print('Indefinido \n')
+                else:
+                    valor = X/Y
+                    print(f'Esse é o resultado da divisão {valor:.2f} \n')
+            else:
+                #O else está aqui para caso o usuario use um operador que não seja alguma das opções oferecidas
+                print('Esse é um operador invalido, por favor tente novamente!')
     except ValueError:
         print('ERRO! digite um valores validos para as opções')
         #Se o codigo chega no except é devido ao usuario não ter digitado um numero em alguma das duas variaveis 
